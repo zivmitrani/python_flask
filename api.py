4641dc93f5415193dc6f3efd95294a75a496e26d
zivmitrani <ziv3688@gmail.com>
2020-10-09 19:35:12 +0300

    added requirements.txt and Dockerfie will dockerize the app

--- api.py
+++ api.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template
 app = Flask(__name__)
-@app.route('/')
+@app.route('/', methods=['GET'])
 def home():
     return render_template('home.html')
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(port=80, debug=True)
