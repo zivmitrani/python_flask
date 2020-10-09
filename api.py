7e255e8f45ebffb9eb45db46c807a9fc6c0c14c2
zivmitrani <ziv3688@gmail.com>
2020-10-09 20:45:55 +0300

    updated hierarchy

--- api.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask, render_template
-app = Flask(__name__)
-@app.route('/', methods=['GET'])
-def home():
-    return render_template('home.html')
-if __name__ == '__main__':
-    app.run(port=80, debug=True)
